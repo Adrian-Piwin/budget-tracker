5e02ba31301fea63ac1c89a8ba6169ed5e8c2d4f
Adrian Piwin <apiwin@miaplaza.com>
2025-03-03 20:02:16 -0500

    Put creds in env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,11 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from 'expo-secure-store'; 
 import { Platform } from 'react-native';
 
 // Use environment variables in a real app
-const supabaseUrl = 'https://noxkunqhombaevcmhltj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5veGt1bnFob21iYWV2Y21obHRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NzMxNzMsImV4cCI6MjA1NjU0OTE3M30.HsYKomsA7rPEl6v2A2YUBdd-HqT_P2x2mBQZpIMMgYc';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 // Custom storage implementation for React Native using Expo's SecureStore
 const ExpoSecureStoreAdapter = {
@@ -31,7 +31,7 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string, {
   auth: {
     storage: ExpoSecureStoreAdapter,
     autoRefreshToken: true,
